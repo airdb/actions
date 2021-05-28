a0193c0fc2ebfe6b8e6d35cecacdf02ce7ff3831
deancn <dean@airdb.com>
2021-05-29 02:33:51 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -10,11 +10,6 @@ RUN apt update && apt upgrade -y && apt install -y curl \
       gpg
 
 	
-#RUN echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" > /etc/apt/sources.list.d/github-cli.list
-
-#ADD https://cli.github.com/packages/githubcli-archive-keyring.gpg /tmp/githubcli-archive-keyring.gpg
-#RUN cat /tmp/githubcli-archive-keyring.gpg  |  gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg && \ apt update && apt install -y gh
-
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 env
-git clone $GITHUB_SERVER_URL/$GITHUB_REPOSITORY
+git clone $GITHUB_SERVER_URL/$GITHUB_REPOSITORY $GITHUB_REPOSITORY
+cd $GITHUB_REPOSITORY
+git checkout $GITHUB_REF
+
 pwd
 ls -lt
 
@@ -10,3 +13,25 @@ apt install -y hugo
 hugo version
 
 echo "Welcome to use dockerfile."
+
+
+function install_gh() {
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+	apt update
+	apt install -y gh
+}
+
+function release_gh_page() {
+	git init
+        git config user.name "airdb-bot"
+        git config user.email "airdb-bot@airdb.com"
+        git add -A
+        git commit -m "Auto Update by actions"
+	gh_pages_branch="gh-pages"
+	if [[ $GITHUB_REF != "refs/heads/main" ]]; then
+		gh_pages_branch="main"
+	fi
+
+        git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}" $gh_pages_branch
+}
